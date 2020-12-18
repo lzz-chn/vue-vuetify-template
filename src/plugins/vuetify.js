28e0f2535c8ca3f632f6e85058056644a8968786
lizhongze <lizhongze.chn@gmail.com>
2020-12-18 15:32:11 +0800

    修复：vuetify-dialog插件已知问题、封装$dialog.message方法

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,10 +13,27 @@ const vuetify = new Vuetify({
 Vue.use(Vuetify)
 Vue.use(VuetifyDialog, {
     context: { vuetify },
-    actions: [
-        { text: '取消', key: false },
-        { text: '确认', color: 'blue', key: true }
-    ]
+    actions: {
+        false: { text: '取消' },
+        true: { text: '确认', color: 'primary' }
+    }
 })
+
+Vue.prototype.$toast = {
+    message: Vue.prototype.$dialog.message,
+    params: { position: 'top', timeout: 3000 },
+    info(msg) {
+        return this.message.info(msg, this.params)
+    },
+    success(msg) {
+        return this.message.success(msg, this.params)
+    },
+    warning(msg) {
+        return this.message.warning(msg, this.params)
+    },
+    error(msg) {
+        return this.message.error(msg, this.params)
+    }
+}
 
 export default vuetify
