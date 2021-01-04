b7208472bc11f1846d0d37af905e1dfeb844dc97
lizhongze <lizhongze.chn@gmail.com>
2021-01-04 09:30:20 +0800

    新增：路由缺省页跳转，包含（路由错误、无权限、开发中）

---
+++ src/components/defaultPage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="default-page">
+        <div class="page-wrapper">
+            <img v-show="type == 'notFound'" class="page-img" src="@/assets/img/default-page/not-found.svg" />
+            <img v-show="type == 'noPower'" class="page-img" src="@/assets/img/default-page/no-power.svg" />
+            <img v-show="type == 'inDevelopment'" class="page-img" src="@/assets/img/default-page/in-development.svg" />
+            <div class="page-tips">
+                {{ tip[type] }}
+                <v-btn text color="primary" @click="$router.push('/admin')">点击跳转</v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'defaultPage',
+    data: () => ({
+        type: '',
+        tip: {
+            notFound: '错误的访问',
+            noPower: '无权限访问',
+            inDevelopment: '开发中敬请期待'
+        }
+    }),
+    mounted() {
+        this.type = this.$route.params.type
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.default-page {
+    display: flex;
+    height: 100vh;
+    .page-wrapper {
+        margin: auto;
+        .page-img {
+            width: 35vw;
+        }
+        .page-tips {
+            margin-top: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            .v-btn {
+                margin: 0 5px;
+            }
+        }
+    }
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -5,29 +5,38 @@ import Home from '../views/Home.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/defaultPage/:type',
+        name: 'defaultPage',
+        component: () => import('@/components/defaultPage') // 缺省页
+    },
+    {
+        path: '*',
+        redirect: '/defaultPage/notFound' // 路由错误无此页面
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes
 })
 
 // 根据meta修改title
 router.beforeEach((to, from, next) => {
-  to.meta.title && (document.title = to.meta.title)
-  next()
+    to.meta.title && (document.title = to.meta.title)
+    next()
 })
 
 export default router
